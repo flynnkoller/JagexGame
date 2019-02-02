db1f1dfaf88643ba90b5f4da56c7c3753e7885f4
DESKTOP-PPFR8GA\Flynn <flynnkoller@hotmail.com>
2019-02-02 17:01:10 +0000

    movement/camera test
    quick demo of how the movement & camera could work

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 3f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move Forward and back along z axis                           
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        //Move Left and right along x Axis                               
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    private const float YangleMin = 0.0f;
+    private const float YangleMax = 50.0f;
+
+    public Transform lookAt;
+    public Transform cameraTransform;
+
+    private Camera camera;
+
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 4.0f;
+    private float sensitivityY = 1.0f;
+
+    private void Start()
+    {
+        cameraTransform = transform;
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += -Input.GetAxis("Mouse Y");
+        currentY = Mathf.Clamp(currentY, YangleMin, YangleMax);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        cameraTransform.position = lookAt.position + rotation * dir;
+        cameraTransform.LookAt(lookAt.position);
+    }
+}
