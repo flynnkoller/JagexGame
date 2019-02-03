b6b147d2a6c0322bbb3b672d1aba42e2bd90ebad
BradleyJohnson99 <47277882+BradleyJohnson99@users.noreply.github.com>
2019-02-03 23:41:21 +0000

    Brad - MoveUpdate
    Added a rotation to the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float moveSpeed = 3f;
+    public Camera cam;
+
     // Use this for initialization
     void Start()
     {
@@ -14,9 +16,18 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
         //Move Forward and back along z axis                           
         transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+
         //Move Left and right along x Axis                               
         transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+
+        //Rotate the player so that forward is the way the camera is facing
+        transform.eulerAngles = new Vector3(0,cam.transform.eulerAngles.y,0);
     }
 }
