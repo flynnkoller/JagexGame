fd449850ba6a108540d2b4aec336289bc9bb1d4d
DESKTOP-PPFR8GA\Flynn <flynnkoller@hotmail.com>
2019-02-07 19:30:59 +0000

    added stairs/dungeon scene

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     public float moveSpeed = 3f;
     public Camera cam;
+    public GameObject player;
 
     // Use this for initialization
     void Start()
@@ -21,6 +24,7 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Move()
     {
+
         //Move Forward and back along z axis                           
         transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
 
@@ -28,6 +32,23 @@ public class PlayerMovement : MonoBehaviour {
         transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
 
         //Rotate the player so that forward is the way the camera is facing
-        transform.eulerAngles = new Vector3(0,cam.transform.eulerAngles.y,0);
+        transform.eulerAngles = new Vector3(0, cam.transform.eulerAngles.y, 0);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        PlayerPrefs.SetFloat("PlayerX", player.transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", player.transform.position.y);
+        PlayerPrefs.SetFloat("PlayerZ", player.transform.position.z);
+        if (collision.gameObject.tag == "DungeonTrigger1")
+        {
+            SceneManager.LoadScene("Dungeon1");
+        }
+        if (collision.gameObject.tag == "HubReturn")
+        {
+            SceneManager.LoadScene("Hub");
+        }
+    }
+
+    
 }
