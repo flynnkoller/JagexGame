93af10896c08a863c479da0e7ddb192255dcc9d3
DESKTOP-PPFR8GA\Flynn <flynnkoller@hotmail.com>
2019-02-09 18:06:57 +0000

    added main menu stuff

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene("Hub");
+    }
+
+    public void ControlsScreen()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void CreditsScreen()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
