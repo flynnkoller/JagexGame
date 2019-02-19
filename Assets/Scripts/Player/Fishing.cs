368af4ba1ea46c3eab947594da62c2afd87e5b19
DESKTOP-PPFR8GA\Flynn <flynnkoller@hotmail.com>
2019-02-19 22:40:08 +0000

    messin about

---
+++ Assets/Scripts/Player/Fishing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Fishing : MonoBehaviour {
+
+    public GameObject prompt;
+
+	// Use this for initialization
+	void Start () {
+        prompt.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pond")
+        {
+
+            Destroy(this);
+            prompt.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Pond")
+        {
+
+            Destroy(this);
+            prompt.SetActive(true);
+        }
+    }
+}
